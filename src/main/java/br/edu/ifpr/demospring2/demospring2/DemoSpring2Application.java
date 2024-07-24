a8cccb1cd576172635e0bf124a01975ef3b7db10
IFPR <ifpr@jacarezinho.ifpr>
2024-07-23 21:55:58 -0300

    projeto criado

---
+++ src/main/java/br/edu/ifpr/demospring2/demospring2/DemoSpring2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.demospring2.demospring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpring2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpr/demospring2/demospring2/DemoSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.demospring2.demospring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
